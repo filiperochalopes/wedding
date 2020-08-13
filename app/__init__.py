8eb68915190cfb92ac45ca063960bf0c08e6d4fa
Filipe Lopes <filiperocklopes@gmail.com>
2020-08-13 11:37:48 -0300

    Implementando configurações de log

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask (__name__)
 app.config.from_object('config')
