be254baf45bed1b76bddf63ff74873568473a2d1
Filipe Lopes <filiperocklopes@gmail.com>
2020-08-26 15:21:03 -0300

    Reconfigurado init para aceitar seervidor não compartilhado

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,19 @@
 from flask import Flask
+from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
-from logging.config import dictConfig
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {
-        'wsgi': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'formatter': 'default',
-            'filename': 'app.log',
-            'maxBytes': 1024,
-            'backupCount': 3,
-            'level': 'DEBUG',
-        }
-    },
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
+from flask_scss import Scss
+from flask_migrate import Migrate
 
 app = Flask (__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+
+Scss(app, static_dir='app/static', asset_dir='app/assets/scss')
 
 from app.models import tables, forms
 from app.controllers import default
