858a48bf5f643fa9a58c624700dc2156f8523433
Filipe Lopes <filiperocklopes@gmail.com>
2020-10-05 20:31:40 -0300

    Adicionado responsividade ao menu

--- app/static/app.js
+++ app/static/app.js
@@ -1,8 +1,17 @@
-$(document).ready( function () {
-    $('#convidados').DataTable();
+$(document).ready(function () {
+  $("#convidados").DataTable();
 
-    $('.teste_alerta').on("click", function (e){
-        console.log(e)
-        alert("Heelo world!")
-    })
-} );
+  $(".teste_alerta").on("click", function (e) {
+    console.log(e);
+    alert("Hello world!");
+  });
+
+  $("#toogle_menu").click(function (e) {
+    console.log($(this));
+    if ($("nav").css("display") === "none") {
+      $("nav").css("display", "block");
+    } else {
+      $("nav").css("display", "none");
+    }
+  });
+});
