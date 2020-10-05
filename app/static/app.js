8c6a253bfe669c3ab44a82ceeb15a01a77513b0e
Filipe Lopes <filiperocklopes@gmail.com>
2020-10-05 20:58:52 -0300

    Relacionando botão de presentear ao link de pagamento pagarme

--- app/static/app.js
+++ app/static/app.js
@@ -14,4 +14,36 @@ $(document).ready(function () {
       $("nav").css("display", "none");
     }
   });
+
+  $(".presentear-bt").click(function (e) {
+    // inicia a instância do checkout
+    var checkout = new PagarMeCheckout.Checkout({
+      encryption_key: "ek_test_qedtchn5pAnzVpEqetVwP86Cw4FgBc",
+      success: function (data) {
+        console.log(data);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+      close: function () {
+        console.log("The modal has been closed.");
+      },
+    });
+
+    console.log(parseFloat($(this).data("price")).toString());
+
+    checkout.open({
+      amount: parseFloat($(this).data("price")).toString() + "00",
+      customerData: "true",
+      createToken: "true",
+      items: [
+        {
+          title: `Valor de ${$(this).data("title")}`,
+          unit_price: $(this).data("price"),
+          quantity: 1,
+          tangible: "false",
+        },
+      ],
+    });
+  });
 });
