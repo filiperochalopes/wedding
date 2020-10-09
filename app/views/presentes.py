46a72b262ac0e911cd08bb40aacd1dc8cd0a90c0
Filipe Lopes <filiperocklopes@gmail.com>
2020-10-09 13:35:00 -0300

    Estilização de lista de presente e adicionado imagem de alianças

--- app/views/presentes.py
+++ app/views/presentes.py
@@ -6,4 +6,5 @@ from app.models.Models import Presente
 @app.route("/presentes")
 def presentes():
     presentes = db.session.query(Presente).all()
+    print(presentes)
     return render_template('presentes.html', presentes=presentes)
