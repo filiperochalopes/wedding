c7b652b73fa24d3d32a7d113cbae2e47fdde1c2b
Filipe Lopes <filiperocklopes@gmail.com>
2021-01-06 21:09:20 -0300

    Reestruturação da ordem dos presentes

--- app/views/presentes.py
+++ app/views/presentes.py
@@ -5,5 +5,5 @@ from app.models.Models import Presente
 
 @app.route("/presentes")
 def presentes():
-    presentes = db.session.query(Presente).all()
+    presentes = db.session.query(Presente).order_by(Presente.fk_categoria.asc(), Presente.preco.asc()).all()
     return render_template('presentes.html', presentes=presentes)
